b43c77215e4c1be1763c3bec96c0961f671e4baf
Cassandra Dacha <cassie.dacha@gmail.com>
2021-03-23 22:21:31 +0200

    Added funtionality to the tag.cpp file to read and parse a specified input file

--- Tag.cpp
+++ Tag.cpp
@@ -10,39 +10,46 @@ struct TagStruct {
   int numOfTags;
   string text;
   
-} tags [3];
+} ;/*tags [3];*/
 
 int main ()
 {
-  /*vector<TagStruct> tags;*/
+  vector<TagStruct> tags;
   ifstream file("simple.txt");
   string str,x,y,z;
   int count;
+  bool found = false;
   while (getline(file, str)) {
-  	/*tags[count].tagName = str.substr(str.find("<")+1,str.find(">")-1);
-  	tags[count].text = str.substr(str.find(">"),str.rfind("<"));
-  	count++;*/
-    //tags.push_back(TagStruct());
     x = str.substr(str.find(">")+1,str.find("/"));
     y = x.substr(0,x.find("<"));
     z = str.substr(str.find("<")+1,str.find(">")-1);
-    count++;
+   /* count++;
     tags[count].tagName = z;
     tags[count].text = y;
     tags[count].numOfTags = 1;
-    cout <<z<< "\n";
-    /*bool found = false;
-    for(int i = 0; i < int(tags.size()); i++) {
-      if(decki.suit == suit_input && decki.face == face_input) {
-         cout << "Card already entered";
+    /*cout <<z<< "\n";
+      cout << tags[count].tagName << "\n";
+      cout << tags[count].text << "\n";
+  
+    */
+   /* for(int i = 0; i < int(tags.size()); i++) {
+      if(tags[i].tagName == z) {
          found = true;
          break;
    }
   }*/
-
-    cout << tags[count].tagName << "\n";
-    cout << tags[count].text << "\n";
-  
+	  if(!found) {
+	   //create struct
+	   TagStruct c;
+	   c.tagName = z;
+	   c.numOfTags = 1;
+	   c.text = y;
+	   tags.push_back(c);
+	  }
+}
+ for (vector<TagStruct>::iterator p = tags.begin(); p != tags.end(); ++p){
+  	cout << p->tagName << " " << p->text << endl;
   }
+ return 0;
 }
 
