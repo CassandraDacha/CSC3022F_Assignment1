5fdd81089ca9a43ffa71dee93915e07cc46728ef
Cassandra Dacha <cassie.dacha@gmail.com>
2021-03-26 14:39:13 +0200

    Added comments to each file

--- Tag.cpp
+++ Tag.cpp
@@ -6,28 +6,34 @@
 
 using namespace std;
 
-
+/*Struct of type TagStuct is created*/
 struct TagStruct {
   string tagName;
   int numOfTags;
   string text;
   
 } ;
-
-void readAndParse(vector<TagStruct> vect) {q
+/*
+Function that takes in a vector that contains elements of type Tagstruct . It then iterates through each element and prints out it's tagname,number of Tags and text it contains
+*/
+void readAndParse(vector<TagStruct> vect) {
  for (vector<TagStruct>::iterator p = vect.begin(); p != vect.end(); ++p){
   	cout <<"\"" << p->tagName << "\""<< "," << p->numOfTags << ","<<"\"" << p->text << "\""<< endl;
   }
   cout<< "\n";
   }
-  
+  /*
+Function that takes in a vector that contains elements of type Tagstruct . It then iterates through each element and prints out it's tagname
+*/
  void printAllTags(vector<TagStruct> vect) {
  for (vector<TagStruct>::iterator p = vect.begin(); p != vect.end(); ++p){
   	cout <<"\"" << p->tagName << "\""<< ", \n";
   }
   cout<< "\n";
   }
-  
+  /*
+Function that takes in a vector that contains elements of type Tagstruct . It then iterates through each element and adds it's tagname,number of Tags and text it contains to a file called Tags.txt
+*/
   void writeTags(vector<TagStruct> vect){
   
   ofstream MyFile("Tags.txt");
--- Tagdriver.cpp
+++ Tagdriver.cpp
@@ -1,5 +1,5 @@
-#include <iostream>   //a system header file - needed to do simple I/O
-#include <fstream>
+#include <iostream>   
+#include <fstream> 
 #include <string>
 #include <vector>
 #include "Tag.h"
@@ -8,36 +8,46 @@ using namespace std;
 
 int main ()
 {
+/*Main function that  reads in the file string by string storing  the tag name in variable tag, text in variable text and updates the number of times a tag is seen in the string */
   vector<TagStruct> tags;
   ifstream file("simple.txt");
-  string str,x,y,z;
+  string str,x,text,tag;
   char input;
   int count;
   bool found = false;
   
+  
+  /*
+  While loop that reads file line by line and stores the tag and text contained in each line
+  */
    while (getline(file, str)) {
     x = str.substr(str.find(">")+1,str.find("/"));
-    y = x.substr(0,x.find("<"));
-    z = str.substr(str.find("<")+1,str.find(">")-1);
+    text = x.substr(0,x.find("<"));
+    tag = str.substr(str.find("<")+1,str.find(">")-1);
  
+ /*
+ For loop that iterates the tag vector and checks is the tagname already exixts in the vector. If it exists, it appends it's text to the existing tag, if it doesn't exist, it creates a Tagstruct object and adds it to the vector
+ */
  
     for(int i = 0; i < int(tags.size()); i++) {
-      if(tags[i].tagName == z) {
+      if(tags[i].tagName == tag) {
          found = true;
-         tags[i].text = tags[i].text + ":" + y ;
+         tags[i].text = tags[i].text + ":" + text ;
          tags[i].numOfTags = tags[i].numOfTags + 1;
          break;
    }
   }
 	  if(!found) {
 	   TagStruct c;
-	   c.tagName = z;
+	   c.tagName = tag;
 	   c.numOfTags = 1;
-	   c.text = y;
+	   c.text = text;
 	   tags.push_back(c);
 	  }
 }
-
+/*
+Loop that prints out the text menu
+*/
 while (true){
  
   cout << "Enter an option (r,p,d,l) or q to quit, and press return... \n";
@@ -48,7 +58,9 @@ while (true){
   cout<< "q: Quit \n";
   cout << "\n";
   cin >> input;
- 
+ /*
+ Switch statement that reads in the input entered by user and responds based on the menu option selected
+ */
   switch(input) {
       case 'r' :
          readAndParse(tags);
